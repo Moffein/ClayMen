53e154b8fa70aaedf123f6a89a5e93cb03d63d31
Moffein <22742925+Moffein@users.noreply.github.com>
2020-12-09 04:32:52 -0800

    Add project files.

---
+++ ClayMen/Class1.cs
@@ -0,0 +1,291 @@
+﻿using BepInEx;
+using RoR2;
+using UnityEngine;
+using RoR2.Navigation;
+using R2API;
+using System.Collections.Generic;
+using RoR2.CharacterAI;
+using R2API.Utils;
+using BepInEx.Configuration;
+
+namespace ClayMen
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.Moffein.ClayMen", "Clay Men", "1.2.1")]
+    [R2API.Utils.R2APISubmoduleDependency(nameof(DirectorAPI), nameof(LanguageAPI), nameof(PrefabAPI))]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+    public class ClayMen : BaseUnityPlugin
+    {
+        private static ConfigEntry<int> spawnCost;
+        private GameObject cm, clayObject;
+        public static Transform headTransform;
+
+        public void Start()
+        {
+            EntityStates.ClaymanMonster.SwipeForward.attackString = "Play_merc_sword_swing";
+            EntityStates.ClaymanMonster.SwipeForward.selfForceMagnitude = 1800f;
+            EntityStates.ClaymanMonster.SwipeForward.baseDuration = 1f;
+            EntityStates.ClaymanMonster.Leap.verticalJumpSpeed = 20f;
+            EntityStates.ClaymanMonster.Leap.horizontalJumpSpeedCoefficient = 2.4f;
+            EntityStates.ClaymanMonster.SpawnState.duration = 3.2f;
+        }
+        public void Awake()
+        {
+            clayObject = PrefabAPI.InstantiateClone(Resources.Load<GameObject>("prefabs/characterbodies/ClayBody"), "MoffeinClayManBody", true);
+            BodyCatalog.getAdditionalEntries += delegate (List<GameObject> list)
+            {
+                list.Add(clayObject);
+            };
+            cm = PrefabAPI.InstantiateClone(Resources.Load<GameObject>("prefabs/charactermasters/ClaymanMaster"), "MoffeinClayManMaster", true);
+            MasterCatalog.getAdditionalEntries += delegate (List<GameObject> list)
+            {
+                list.Add(cm);
+            };
+            spawnCost = base.Config.Bind<int>(new ConfigDefinition("General Settings", "01 - Spawn Cost"), 15, new ConfigDescription("How many director credits Clay Men take to spawn."));
+
+            CharacterSpawnCard beetleCSC = Resources.Load<CharacterSpawnCard>("SpawnCards/CharacterSpawnCards/cscBeetle");
+            //CharacterSpawnCard clayBossCSC = Resources.Load<CharacterSpawnCard>("SpawnCards/CharacterSpawnCards/cscClayBoss");
+
+            ModifyClayMan();
+            ItemDisplays.DisplayRules(clayObject);
+
+            CharacterSpawnCard clayManCSC = ScriptableObject.CreateInstance<CharacterSpawnCard>();
+            clayManCSC.name = "cscClayMan";
+            clayManCSC.prefab = cm;
+            clayManCSC.sendOverNetwork = true;
+            clayManCSC.hullSize = HullClassification.Human;
+            clayManCSC.nodeGraphType = MapNodeGroup.GraphType.Ground;
+            clayManCSC.requiredFlags = NodeFlags.None;
+            clayManCSC.forbiddenFlags = NodeFlags.NoCharacterSpawn;
+            clayManCSC.directorCreditCost = spawnCost.Value;
+            clayManCSC.occupyPosition = false;
+            clayManCSC.loadout = new SerializableLoadout();
+            clayManCSC.noElites = false;
+            clayManCSC.forbiddenAsBoss = false;
+
+            DirectorCard clayManDC = new DirectorCard
+            {
+                spawnCard = clayManCSC,
+                selectionWeight = 1,
+                allowAmbushSpawn = true,
+                preventOverhead = false,
+                minimumStageCompletions = 0,
+                requiredUnlockable = "",
+                forbiddenUnlockable = "",
+                spawnDistance = DirectorCore.MonsterSpawnDistance.Close
+            };
+            DirectorAPI.DirectorCardHolder clayManCard = new DirectorAPI.DirectorCardHolder
+            {
+                Card = clayManDC,
+                MonsterCategory = DirectorAPI.MonsterCategory.BasicMonsters,
+                InteractableCategory = DirectorAPI.InteractableCategory.None
+            };
+
+            /*DirectorAPI.FamilyActions += delegate (List<DirectorAPI.MonsterFamilyHolder> list, DirectorAPI.StageInfo stage)
+            {
+                foreach (DirectorAPI.MonsterFamilyHolder holder in list)
+                {
+                    foreach (DirectorCard dC in holder.FamilyChampions)
+                    {
+                        if (dC.spawnCard == clayBossCSC)
+                        {
+                            if (!holder.FamilyBasicMonsters.Contains(clayManDC))
+                            {
+                                holder.FamilyBasicMonsters.Add(clayManDC);
+                            }
+                        }
+                    }
+                }
+            };*/
+
+            DirectorAPI.MonsterActions += delegate (List<DirectorAPI.DirectorCardHolder> list, DirectorAPI.StageInfo stage)
+            {
+                bool addClayMan = false;
+                switch (stage.stage)
+                {
+                    case DirectorAPI.Stage.ArtifactReliquary:
+                    case DirectorAPI.Stage.VoidCell:
+                    case DirectorAPI.Stage.AbandonedAqueduct:
+                    case DirectorAPI.Stage.RallypointDelta:
+                        addClayMan = true;
+                        break;
+                    case DirectorAPI.Stage.ScorchedAcres:
+                        addClayMan = true;
+                        foreach (DirectorAPI.DirectorCardHolder dc in list)
+                        {
+                            if (dc.Card.spawnCard == beetleCSC)
+                            {
+                                dc.Card.selectionWeight = 0;
+                            }
+                        }
+                        break;
+                    case DirectorAPI.Stage.Custom:
+                        if (stage.CustomStageName == "rootjungle")
+                        {
+                            addClayMan = true;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                if (addClayMan)
+                {
+                    if (!list.Contains(clayManCard))
+                    {
+                        list.Add(clayManCard);
+                    }
+                }
+            };
+            On.EntityStates.ClaymanMonster.SpawnState.OnEnter += (orig, self) =>
+            {
+                orig(self);
+                Util.PlayScaledSound("Play_clayBruiser_attack2_shoot", self.outer.gameObject, 1f);
+            };
+        }
+
+        private void ModifyClayMan()
+        {
+            
+            AISkillDriver clayPrimary = cm.GetComponent<AISkillDriver>();
+            clayPrimary.maxDistance = 16f;
+            cm.GetComponent<CharacterMaster>().bodyPrefab = clayObject;
+
+            LanguageAPI.Add("CLAY_BODY_NAME", "Clay Man");
+
+            LanguageAPI.Add("CLAY_BODY_LORE", "<style=cMono>\r\n$ Transcribing image... done.\r\n$ Resolving... done.\r\n$ Outputting text strings... done.\r\nComplete!\r\n</style>\r\n\r\nField Notes:  Quick with his sword and quicker with his feet; the agility of these clay 'people' is unexpected with a form so roughly shaped.\n\nWhen faced with one of the few creatures here which I feel some humanity in, my aloneness closes in. Why do they have clay pots on their heads? Could it be protection from this cruel reality, or maybe just to hide the scars from this brutal planet.");
+            clayObject.AddComponent<Interactor>().maxInteractionDistance = 3f;
+            clayObject.AddComponent<InteractionDriver>();
+
+            ModelLocator clayModelLocator = clayObject.GetComponent<ModelLocator>();
+            clayModelLocator.modelTransform.gameObject.layer = LayerIndex.entityPrecise.intVal;
+            clayModelLocator.modelTransform.localScale *= 1.2f;
+            clayModelLocator.noCorpse = true;
+
+            CharacterDeathBehavior clayCDB = clayObject.GetComponent<CharacterDeathBehavior>();
+            clayCDB.deathState = Resources.Load<GameObject>("prefabs/characterbodies/WispBody").GetComponent<CharacterDeathBehavior>().deathState;
+
+            CharacterBody clayCB = clayObject.GetComponent<CharacterBody>();
+            clayCB.baseNameToken = "CLAY_BODY_NAME";
+            clayCB.baseJumpPower = 22f;
+            clayCB.baseMaxHealth = 170f;
+            clayCB.levelMaxHealth = clayCB.baseMaxHealth*0.3f;
+            clayCB.baseArmor = 0f;
+            clayCB.baseDamage = 12f; //14 -> 12
+            clayCB.levelDamage = clayCB.baseDamage*0.2f;
+            clayCB.baseMoveSpeed = 9f;
+            clayCB.baseRegen = 0f;
+            clayCB.levelRegen = 0f;
+            clayCB.bodyFlags = CharacterBody.BodyFlags.ImmuneToGoo;
+
+            Debug.Log(clayCB.GetComponent<DeathRewards>().logUnlockableName);
+            clayCB.GetComponent<DeathRewards>().logUnlockableName = "Logs.ClayBody.0";
+
+            SetStateOnHurt claySSoH = clayObject.AddComponent<SetStateOnHurt>();
+            claySSoH.canBeFrozen = true;
+            claySSoH.canBeStunned = true;
+            claySSoH.canBeHitStunned = false;
+            claySSoH.hitThreshold = 0.15f;
+
+            SfxLocator claySFX = clayObject.GetComponent<SfxLocator>();
+            claySFX.deathSound = "Play_clayboss_M1_explo";
+            claySFX.barkSound = "";
+
+            //Ice Fix Credits: SushiDev
+            int i = 0;
+            EntityStateMachine[] esmr = new EntityStateMachine[2];
+            foreach (EntityStateMachine esm in clayObject.GetComponentsInChildren<EntityStateMachine>())
+            {
+                switch (esm.customName)
+                {
+                    case "Body":
+                        claySSoH.targetStateMachine = esm;
+                        break;
+                    default:
+                        if (i < 2)
+                        {
+                            esmr[i] = esm;
+                        }
+                        i++;
+                        break;
+                }
+            }
+
+            #region hitbox
+            Component[] clayComponents = clayObject.GetComponentsInChildren<Transform>();
+            Transform clayTransform = null;
+            Transform clayHeadTransform = null;
+            Transform claySwordHitboxTransform = null;
+            foreach (Transform t in clayComponents)
+            {
+                if (t.name == "chest")
+                {
+                    clayTransform = t;
+                }
+                else if (t.name == "head")
+                {
+                    clayHeadTransform = t;
+                }
+                else if (t.name == "Hitbox")
+                {
+                    claySwordHitboxTransform = t;
+                }
+                if (clayTransform != null && clayHeadTransform != null && claySwordHitboxTransform != null)
+                {
+                    break;
+                }
+            }
+
+            ItemDisplays.headTransform = clayHeadTransform;
+
+            HurtBoxGroup clayHurtBoxGroup = clayModelLocator.modelTransform.gameObject.AddComponent<HurtBoxGroup>();
+            claySwordHitboxTransform.localScale *= 2.4f; //2.8 -> 2.4
+
+            #region chest
+            clayTransform.gameObject.layer = LayerIndex.entityPrecise.intVal;
+            CapsuleCollider clayCollider = clayTransform.gameObject.AddComponent<CapsuleCollider>();
+            clayCollider.center -= new Vector3(0, 0.6f, 0);
+            clayCollider.height *= 0.25f;
+            clayCollider.radius *= 1.16f;
+            HurtBox clayHurtBox = clayTransform.gameObject.AddComponent<HurtBox>();
+            clayHurtBox.isBullseye = true;
+            clayHurtBox.healthComponent = clayObject.GetComponent<HealthComponent>();
+            clayHurtBox.damageModifier = HurtBox.DamageModifier.Normal;
+            clayHurtBox.hurtBoxGroup = clayHurtBoxGroup;
+            clayHurtBox.indexInGroup = 0;
+            //clayHurtBox.name = "ChestHurtbox";
+            #endregion
+
+            #region head
+
+
+            clayHeadTransform.gameObject.layer = LayerIndex.entityPrecise.intVal; 
+            CapsuleCollider clayHeadCollider = clayHeadTransform.gameObject.AddComponent<CapsuleCollider>();
+            clayHeadCollider.height *= 0.4f;
+            clayHeadCollider.radius *= 0.3f;
+            clayHeadCollider.center += new Vector3(0, 0.2f, 0);
+            HurtBox clayHeadHurtBox = clayHeadTransform.gameObject.AddComponent<HurtBox>();
+            clayHeadHurtBox.isBullseye = false;
+            clayHeadHurtBox.healthComponent = clayObject.GetComponent<HealthComponent>();
+            clayHeadHurtBox.damageModifier = HurtBox.DamageModifier.SniperTarget;
+            clayHeadHurtBox.hurtBoxGroup = clayHurtBoxGroup;
+            clayHeadHurtBox.indexInGroup = 1;
+            //clayHeadHurtBox.name = "HeadHurtbox";
+
+            #endregion
+
+            HurtBox[] clayHurtBoxArray = new HurtBox[]
+            {
+                clayHurtBox, clayHeadHurtBox
+            };
+
+            clayHurtBoxGroup.bullseyeCount = 1;
+            clayHurtBoxGroup.hurtBoxes = clayHurtBoxArray;
+            clayHurtBoxGroup.mainHurtBox = clayHurtBox;
+
+            #endregion
+
+            EntityLocator clayLocator = clayObject.AddComponent<EntityLocator>();
+            clayLocator.entity = clayObject;
+        }
+    }
+}
---
+++ ClayMen/ItemDisplays.cs
@@ -0,0 +1,196 @@
+﻿using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Reflection;
+using UnityEngine;
+using R2API.Utils;
+
+namespace ClayMen
+{
+    class ItemDisplays
+    {
+        public static List<ItemDisplayRuleSet.NamedRuleGroup> equipmentList;
+        public static Transform headTransform;
+        public static void DisplayRules(GameObject clayObject)
+        {
+            ChildLocator cl = clayObject.GetComponentInChildren<ChildLocator>();
+            Array.Resize(ref cl.transformPairs, cl.transformPairs.Length + 1);
+            cl.transformPairs[cl.transformPairs.Length - 1] = new ChildLocator.NameTransformPair
+            {
+                name = "Head",
+                transform = headTransform
+            };
+
+            ItemDisplayRuleSet idrsCommando = Resources.Load<GameObject>("Prefabs/CharacterBodies/CommandoBody").GetComponent<ModelLocator>().modelTransform.GetComponent<CharacterModel>().itemDisplayRuleSet;
+            ItemDisplayRuleSet.NamedRuleGroup[] ersCommando = idrsCommando.GetFieldValue<ItemDisplayRuleSet.NamedRuleGroup[]>("namedEquipmentRuleGroups");
+            GameObject bluePrefab = null;
+            GameObject redPrefab = null;
+            GameObject whitePrefab = null;
+            GameObject poisonPrefab = null;
+            GameObject ghostPrefab = null;
+            for (int i = 0; i < ersCommando.Length; i++)
+            {
+                if (ersCommando[i].name == "AffixPoison")
+                {
+                    poisonPrefab = ersCommando[i].displayRuleGroup.rules[0].followerPrefab;
+                }
+                else if (ersCommando[i].name == "AffixHaunted")
+                {
+                    ghostPrefab = ersCommando[i].displayRuleGroup.rules[0].followerPrefab;
+                }
+                else if (ersCommando[i].name == "AffixBlue")
+                {
+                    bluePrefab = ersCommando[i].displayRuleGroup.rules[0].followerPrefab;
+                }
+                else if (ersCommando[i].name == "AffixRed")
+                {
+                    redPrefab = ersCommando[i].displayRuleGroup.rules[0].followerPrefab;
+                }
+                else if (ersCommando[i].name == "AffixWhite")
+                {
+                    whitePrefab = ersCommando[i].displayRuleGroup.rules[0].followerPrefab;
+                }
+                if (false)
+                {
+                    Debug.Log("\nChildname: " + ersCommando[i].displayRuleGroup.rules[0].childName + "\nScale: " + ersCommando[i].displayRuleGroup.rules[0].localScale + "\nAngles: " + ersCommando[i].displayRuleGroup.rules[0].localAngles + "\nPos: " + ersCommando[i].displayRuleGroup.rules[0].localPos + "\n\n");
+                }
+            }
+
+            ItemDisplayRuleSet idrsClay = ScriptableObject.CreateInstance<ItemDisplayRuleSet>();
+
+            equipmentList = new List<ItemDisplayRuleSet.NamedRuleGroup>();
+            equipmentList.Add(new ItemDisplayRuleSet.NamedRuleGroup
+            {
+                name = "AffixPoison",
+                displayRuleGroup = new DisplayRuleGroup
+                {
+                    rules = new ItemDisplayRule[]
+                    {
+                        new ItemDisplayRule
+                        {
+                            ruleType = ItemDisplayRuleType.ParentedPrefab,
+                            followerPrefab = poisonPrefab,
+                            childName = "Head",
+                            localPos = new Vector3(0f, 0.3f, 0f),
+                            localAngles = new Vector3(255f, 0f, 0f),
+                            localScale = 0.07f * Vector3.one,
+                            limbMask = LimbFlags.None
+                        }
+                    }
+                }
+            });
+
+            equipmentList.Add(new ItemDisplayRuleSet.NamedRuleGroup
+            {
+                name = "AffixHaunted",
+                displayRuleGroup = new DisplayRuleGroup
+                {
+                    rules = new ItemDisplayRule[]
+                    {
+                        new ItemDisplayRule
+                        {
+                            ruleType = ItemDisplayRuleType.ParentedPrefab,
+                            followerPrefab = ghostPrefab,
+                            childName = "Head",
+                            localPos = new Vector3(0f, 0.3f, 0f),
+                            localAngles = new Vector3(260f, 0f, 0f),
+                            localScale = 0.07f * Vector3.one,
+                            limbMask = LimbFlags.None
+                        }
+                    }
+                }
+            });
+
+            equipmentList.Add(new ItemDisplayRuleSet.NamedRuleGroup
+            {
+                name = "AffixWhite",
+                displayRuleGroup = new DisplayRuleGroup
+                {
+                    rules = new ItemDisplayRule[]
+                    {
+                        new ItemDisplayRule
+                        {
+                            ruleType = ItemDisplayRuleType.ParentedPrefab,
+                            followerPrefab = whitePrefab,
+                            childName = "Head",
+                            localPos = new Vector3(0f, 0.33f, -0.05f),
+                            localAngles = new Vector3(260f, 0f, 0f),
+                            localScale = 0.035f * Vector3.one,
+                            limbMask = LimbFlags.None
+                        }
+                    }
+                }
+            });
+
+            equipmentList.Add(new ItemDisplayRuleSet.NamedRuleGroup
+            {
+                name = "AffixBlue",
+                displayRuleGroup = new DisplayRuleGroup
+                {
+                    rules = new ItemDisplayRule[]
+                    {
+                        new ItemDisplayRule
+                        {
+                            ruleType = ItemDisplayRuleType.ParentedPrefab,
+                            followerPrefab = bluePrefab,
+                            childName = "Head",
+                            localPos = new Vector3(0f, 0.28f, 0.1f),
+                            localAngles = new Vector3(-20f, 0f, 0f),
+                            localScale = 0.18f * Vector3.one,
+                            limbMask = LimbFlags.None
+                        },
+                        new ItemDisplayRule
+                        {
+                            ruleType = ItemDisplayRuleType.ParentedPrefab,
+                            followerPrefab = bluePrefab,
+                            childName = "Head",
+                            localPos = new Vector3(0f, 0.21f, 0.1f),
+                            localAngles = new Vector3(-10f, 0f, 0f),
+                            localScale = 0.25f * Vector3.one,
+                            limbMask = LimbFlags.None
+                        }
+                    }
+                }
+            });
+
+            equipmentList.Add(new ItemDisplayRuleSet.NamedRuleGroup
+            {
+                name = "AffixRed",
+                displayRuleGroup = new DisplayRuleGroup
+                {
+                    rules = new ItemDisplayRule[]
+                    {
+                        new ItemDisplayRule
+                        {
+                            ruleType = ItemDisplayRuleType.ParentedPrefab,
+                            followerPrefab = redPrefab,
+                            childName = "Head",
+                            localPos = new Vector3(-0.1f, 0.25f,-0.1f),
+                            localAngles = new Vector3(0f, 20f, 0f),
+                            localScale = 0.1f * Vector3.one,
+                            limbMask = LimbFlags.None
+                        },
+                        new ItemDisplayRule
+                        {
+                            ruleType = ItemDisplayRuleType.ParentedPrefab,
+                            followerPrefab = redPrefab,
+                            childName = "Head",
+                            localPos = new Vector3(0.1f, 0.25f, -0.1f),
+                            localAngles = new Vector3(0f, -20f, 0f),
+                            localScale = 0.1f * Vector3.one,
+                            limbMask = LimbFlags.None
+                        }
+                    }
+                }
+            });
+
+            BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+            ItemDisplayRuleSet.NamedRuleGroup[] equipmentArray = equipmentList.ToArray();
+            typeof(ItemDisplayRuleSet).GetField("namedEquipmentRuleGroups", bindingAttr).SetValue(idrsClay, equipmentArray);
+
+            clayObject.GetComponent<ModelLocator>().modelTransform.GetComponent<CharacterModel>().itemDisplayRuleSet = idrsClay;
+        }
+    }
+}
